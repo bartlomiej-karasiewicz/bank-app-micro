79b614b451f279cd1f385d4d595e3bb175007d61
Bartlomiej Karasiewicz <bartlomiej.karasiewicz7@gmail.com>
2020-01-18 13:21:18 +0100

    Initialize commit

---
+++ src/main/java/pl/com/example/bankappmicro/BankAppMicroApplication.java
@@ -0,0 +1,13 @@
+package pl.com.example.bankappmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/example/bankappmicro/BankAppMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.example.bankappmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
